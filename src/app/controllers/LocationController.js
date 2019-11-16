00faca87ad3ad2abb2663178e77c6d421ff5cdb3
GustavoConstantini <gustavoconstantini.21@outlook.com>
2019-11-16 01:46:45 -0300

    all bugs fixed

--- src/app/controllers/LocationController.js
+++ src/app/controllers/LocationController.js
@@ -4,13 +4,7 @@ class LocationController {
   async store(req, res) {
     const user = await User.findByPk(req.userId);
 
-    const { latitude, longitude } = user;
-
     await user.update(req.body);
-
-    if (!latitude && !longitude) {
-      return res.status(400).json({ error: 'Falha ao validar' });
-    }
 
     return res.status(200).json({ ok: 'true' });
   }
