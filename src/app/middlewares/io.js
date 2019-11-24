9029f600b47af20c3cbebefd80ef1f7d667b77ba
GustavoConstantini <gustavoconstantini.21@outlook.com>
2019-11-24 02:02:22 -0300

    alpha

--- src/app/middlewares/io.js
+++ src/app/middlewares/io.js
@@ -4,7 +4,8 @@ import { promisify } from 'util';
 import authConfig from '../../config/auth';
 
 export default async (socket, next) => {
-  if (!(socket.handshake.query.token || socket.query.user)) {
+  console.log(socket.handshake.query.token || socket.handshake.query.user);
+  if (!(socket.handshake.query.token || socket.handshake.query.user)) {
     return next(new Error('falha ao validar'));
   }
   try {
@@ -14,6 +15,6 @@ export default async (socket, next) => {
 
     return next();
   } catch (error) {
-    next(new Error('falha ao validar'));
+    return next(new Error('falha ao validar'));
   }
 };
