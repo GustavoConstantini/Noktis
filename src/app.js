11577e33b49ddade71166a3c6a59f7cea1515676
GustavoConstantini <gustavoconstantini.21@outlook.com>
2019-11-24 02:21:15 -0300

    alpha

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ class App {
 
     this.connectedUsers = {};
     this.io.on('connection', (socket) => {
+      console.log('deu certo', socket.id);
       const { user } = socket.handshake.query;
 
       this.connectedUsers[user] = socket.id;
