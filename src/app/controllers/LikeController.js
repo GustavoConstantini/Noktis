71587b5c0351748f87ef3d8d5918e1ec7aea9f94
GustavoConstantini <gustavoconstantini.21@outlook.com>
2019-11-25 21:40:47 -0300

    like controller

--- src/app/controllers/LikeController.js
+++ src/app/controllers/LikeController.js
@@ -17,6 +17,11 @@ class LikeController {
 
       const { dataValues: targetUser } = await User.findOne({ where: { id }, attributes: { exclude: ['password_hash', 'email', 'createdAt', 'updatedAt'] } });
 
+      await loggedUser.update(
+        { likes: sequelize.fn('array_append', sequelize.col('likes'), targetUser.id) },
+        { where: { id: req.userId } },
+      );
+
       if (targetUser.likes !== null) {
         if (targetUser.likes.includes(loggedUser.id)) {
           if (loggedUser.socket) {
@@ -28,16 +33,10 @@ class LikeController {
           }
         }
       }
-
-      await loggedUser.update(
-        { likes: sequelize.fn('array_append', sequelize.col('likes'), targetUser.id) },
-        { where: { id: req.userId } },
-      );
+      return res.status(200).json({ ok: true });
     } catch (error) {
       return res.status(400).json({ error: 'O usuário não existe' });
     }
-
-    return res.status(200).json({ ok: true });
   }
 }
 
