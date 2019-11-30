f80ef837d9e09bb0170c4abeabd49ddd78b5e2a9
GustavoConstantini <gustavoconstantini.21@outlook.com>
2019-11-30 09:20:06 -0300

    bug fixed in socket

--- src/app/functions/onlineConnection.js
+++ src/app/functions/onlineConnection.js
@@ -1,19 +1,23 @@
 import User from '../models/User';
 
-export default async (socket) => {
-  const user = await User.findByPk(socket.userId);
-  user.online = true;
+export default async (Socket) => {
+  try {
+    const user = await User.findByPk(Socket.userId);
+    user.online = true;
 
-  user.socket = socket.id;
- 
-  await user.save();
+    user.socket = Socket.id;
 
-  socket.on('disconnect', async () => {
-    const user = await User.findByPk(socket.userId);
-    user.online = false;
-  
-    user.socket = null;
-   
-    await user.save(); 
-  });
+    await user.save();
+
+    Socket.on('disconnect', async () => {
+      user.online = false;
+
+      user.socket = null;
+
+      await user.save();
+    });
+  } catch (Error) {
+    return new (Error)();
+  }
+  return this;
 };
